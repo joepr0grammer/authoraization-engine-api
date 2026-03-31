ed9c6e727de1102345bb8da3b7de66185ccbf31b
Joe Wabick <joe_wabick@yahoo.com>
2026-03-30 20:15:39 -0500

    Update CORS for production Vercel domain

--- main.py
+++ main.py
@@ -29,7 +29,10 @@ security = HTTPBearer()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000", 
+        "https://authoraization-engine-web.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
